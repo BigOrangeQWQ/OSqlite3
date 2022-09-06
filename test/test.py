9ec96f35290fc2177c1c4653cdd9bb12628a1cdf
BigOrangeQWQ <2284086963@qq.com>
2022-09-06 22:11:50 +0800

    init

--- test/test.py
+++ test/test.py
@@ -8,7 +8,3 @@ from math import pi, sin, tan
 a = int(input("输入边"))
 b = int(input("输入半径"))
 print(f"答案{a*b*b/4*tan(pi/a)}") 
-
- git config --global user.name "BigOrangeQWQ"
- git config --global user.email "2284086963@qq.com"
- ssh-keygen -t rsa -C "2284086963@qq.com"
