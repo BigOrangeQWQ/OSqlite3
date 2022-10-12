0919c2fa585435a995653498f311ee1477225f99
BigOrangeQWQ <63897047+BigOrangeQWQ@users.noreply.github.com>
2022-10-12 14:50:49 +0800

    update
    some function reload

--- src/model.py
+++ src/model.py
@@ -1,12 +1,18 @@
-from typing import Any
+from typing import Any, Dict
 
 
 class Model:
-    _values = {}
+    _values = {'args': tuple(),
+               'logic': Dict[Any, str],}
 
     def __init__(self, *args, **kwargs):
-        self._kwargs: dict = kwargs
-        self._args: tuple = args
+#         self.kwargs: dict = kwargs  
+        _values.update(kwargs)
+        _values['args'] = args
+#         self.args: tuple = args
 
-    def __setattr__(self, __name: str, __value: Any):
-        self._values[__name] = __value
+#     def __setattr__(self, __name: str, __value: Any):
+#         self._values[__name] = __value
+        
+    def __eq__(self, __o: Any):
+        self._values['logic'][__o] == f' == __o'
